f1ab4ac85e9e4295173b6a370a8750edc7a0a224
BrownBear85 <91921122+BrownBear85@users.noreply.github.com>
2025-10-26 13:35:09 -0700

    cobblemon compat

--- src/main/java/com/bonker/stardewfishing/proxy/CobblemonProxy.java
+++ src/main/java/com/bonker/stardewfishing/proxy/CobblemonProxy.java
@@ -1,8 +1,8 @@
 package com.bonker.stardewfishing.proxy;
 
-import com.cobblemon.mod.common.api.spawning.SpawnBucket;
-import com.cobblemon.mod.common.entity.fishing.PokeRodFishingBobberEntity;
-import com.cobblemon.mod.common.item.interactive.PokerodItem;
+//import com.cobblemon.mod.common.api.spawning.SpawnBucket;
+//import com.cobblemon.mod.common.entity.fishing.PokeRodFishingBobberEntity;
+//import com.cobblemon.mod.common.item.interactive.PokerodItem;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.Mth;
@@ -11,17 +11,17 @@ import net.minecraft.world.item.ItemStack;
 
 public class CobblemonProxy {
     public static void spawnPokemon(FishingHook hook, ServerPlayer player, Object obj, ItemStack fishingRod) {
-        if (!(hook instanceof PokeRodFishingBobberEntity pokeHook) || !(obj instanceof SpawnBucket bucket)) {
-            return;
-        }
-
-        if (pokeHook.spawnPokemonFromFishing(player, bucket, fishingRod)) {
-            PokerodItem.Companion.consumeBait(fishingRod);
-        }
-
-        float angle = Mth.nextFloat(hook.getRandom(), 0, 360) * Mth.DEG_TO_RAD;
-        float dist = Mth.nextFloat(hook.getRandom(), 25, 60);
-        double x = hook.getX() + (Mth.sin(angle) * dist) * 0.1;
-        player.serverLevel().sendParticles(ParticleTypes.SPLASH, x, hook.getY(), hook.getZ(), 6 + hook.getRandom().nextInt(4), 0.0, 0.2, 0, 0);
+//        if (!(hook instanceof PokeRodFishingBobberEntity pokeHook) || !(obj instanceof SpawnBucket bucket)) {
+//            return;
+//        }
+//
+//        if (pokeHook.spawnPokemonFromFishing(player, bucket, fishingRod)) {
+//            PokerodItem.Companion.consumeBait(fishingRod);
+//        }
+//
+//        float angle = Mth.nextFloat(hook.getRandom(), 0, 360) * Mth.DEG_TO_RAD;
+//        float dist = Mth.nextFloat(hook.getRandom(), 25, 60);
+//        double x = hook.getX() + (Mth.sin(angle) * dist) * 0.1;
+//        player.serverLevel().sendParticles(ParticleTypes.SPLASH, x, hook.getY(), hook.getZ(), 6 + hook.getRandom().nextInt(4), 0.0, 0.2, 0, 0);
     }
 }
